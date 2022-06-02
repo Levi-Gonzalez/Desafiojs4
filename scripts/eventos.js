261073e61bc581387b7d966ee69aa69b6c5dca69
levi-gonzalez <levigonzalez.t@gmail.com>
2022-06-02 07:34:18 +0200

    eventos

---
+++ scripts/eventos.js
@@ -0,0 +1,16 @@
+const lista = document.getElementById("lista");
+const btnAgregar = document.getElementById('btn-agregar');
+
+lista.addEventListener("click"), (e) => {
+    console.log(e);
+};
+
+btnAgregar.addEventListener('click', () => {
+	const elemento = `
+		<a href="#">
+			Elemento <i class="bi bi-check-square-fill"></i>
+		</a>
+	`;
+
+	lista.innerHTML += elemento;
+});
